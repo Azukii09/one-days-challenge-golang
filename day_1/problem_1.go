f4feda06e0ea115ecf196bd83866331b82eaceb3
Azukii09 <azuknisianta@gmail.com>
2024-05-07 18:12:49 +0700

    add comment in day one challenge

--- day_1/problem_1.go
+++ day_1/problem_1.go
@@ -5,24 +5,9 @@ import (
 	"strings"
 )
 
-/*
-PairNumberForGivenSum
-Problem day 1
-Find a pair with the given sum in an array
-Given an unsorted integer array, find a pair with the given sum in it.
-
-For example,
-
-Input: nums = [8, 7, 2, 5, 3, 1]
-target = 10
-Output: Pair found (8, 2)orPair found (7, 3)
-
-Input: nums = [5, 2, 6, 8, 1, 9]
-target = 12
-Output: Pair not found
-*/
 func PairNumberForGivenSum(target int, nums []int) string {
 	var output [][]int
+	//loop for get output of pair
 	for index, n := range nums {
 		for i := index + 1; i < len(nums); i++ {
 			if index != i {
@@ -32,6 +17,8 @@ func PairNumberForGivenSum(target int, nums []int) string {
 			}
 		}
 	}
+
+	//print the output
 	if len(output) != 0 {
 		var stringOutput string
 		for indexFor, data := range output {
