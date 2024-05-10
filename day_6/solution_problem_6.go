461fb54caf38bbe6d9a321b9b2c2eadc1f208d33
Azukii09 <azuknisianta@gmail.com>
2024-05-10 21:02:04 +0700

    add solution for problem day 6

--- day_6/solution_problem_6.go
+++ day_6/solution_problem_6.go
@@ -1,6 +1,29 @@
 package day_6
 
 func DNACheckValue(dna string) (DNA, string) {
-
-	return DNA{}, ""
+	result := DNA{
+		A: 0,
+		C: 0,
+		G: 0,
+		T: 0,
+	}
+	for i := 0; i < len(dna); i++ {
+		switch dna[i] {
+		case 'A':
+			result.A += 1
+			break
+		case 'C':
+			result.C += 1
+			break
+		case 'G':
+			result.G += 1
+			break
+		case 'T':
+			result.T += 1
+			break
+		default:
+			return result, "error"
+		}
+	}
+	return result, ""
 }
