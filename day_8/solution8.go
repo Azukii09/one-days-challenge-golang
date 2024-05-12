8f1f9fbea6fb397618f7b2d97503f58c2db4ebd1
Azukii09 <azuknisianta@gmail.com>
2024-05-12 18:43:33 +0700

    add solution problem day 8

--- day_8/solution8.go
+++ day_8/solution8.go
@@ -1,6 +1,18 @@
 package day_8
 
 func IsSubset(first, second []Flight) bool {
-
+	set := make(map[Flight]int)
+	for _, flight2 := range second {
+		set[flight2] += 1
+	}
+	for _, flight1 := range first {
+		if count, found := set[flight1]; !found {
+			return false
+		} else if count < 1 {
+			return false
+		} else {
+			set[flight1] = count - 1
+		}
+	}
 	return true
 }
