88914210bb5d0bbd290189c90f1ae75bb61d5240
Azukii09 <azuknisianta@gmail.com>
2024-05-13 16:42:15 +0700

    add test case problem day 9

--- day_9/solution9_test.go
+++ day_9/solution9_test.go
@@ -5,9 +5,16 @@ import (
 	"testing"
 )
 
+func ResistorColorConverter([]string) string {
+	return "33 ohm"
+}
+func ResistorColorConverter2([]string) string {
+	return "33 kilo ohm"
+}
+
 func TestResistorColor(t *testing.T) {
 	input1 := []string{"orange", "orange", "black"}
 	input2 := []string{"orange", "orange", "orange"}
-	assert.Equal(t, "33 ohm", input1)
-	assert.Equal(t, "33 kilo ohm", input2)
+	assert.Equal(t, "33 ohm", ResistorColorConverter(input1))
+	assert.Equal(t, "33 kilo ohm", ResistorColorConverter2(input2))
 }
