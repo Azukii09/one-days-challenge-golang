d593d5715da0fc5d018f3a8202fd11d5dbf84456
Azukii09 <azuknisianta@gmail.com>
2024-05-16 16:35:50 +0700

    add solution day 12

--- day_12/solution12_test.go
+++ day_12/solution12_test.go
@@ -6,8 +6,24 @@ import (
 )
 
 func TestRomanNumber(t *testing.T) {
-	assert.Equal(t, "CV", RomanNumbers(105))
-	assert.Equal(t, "CVI", RomanNumbers(106))
-	assert.Equal(t, "CIV", RomanNumbers(104))
-	assert.Equal(t, "MCMXCVI", RomanNumbers(1996))
+	numbers1, err1 := RomanNumbers(105)
+	if err1 != nil {
+		return
+	}
+	numbers2, err2 := RomanNumbers(106)
+	if err2 != nil {
+		return
+	}
+	numbers3, err3 := RomanNumbers(104)
+	if err3 != nil {
+		return
+	}
+	numbers4, err4 := RomanNumbers(1996)
+	if err4 != nil {
+		return
+	}
+	assert.Equal(t, "CV", numbers1)
+	assert.Equal(t, "CVI", numbers2)
+	assert.Equal(t, "CIV", numbers3)
+	assert.Equal(t, "MCMXCVI", numbers4)
 }
