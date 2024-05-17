33ed59fc1cd768ca77246afa8dcafc468c76c2e1
Azukii09 <azuknisianta@gmail.com>
2024-05-17 16:58:44 +0700

    add solution for problem day 13

--- day_13/solution13.go
+++ day_13/solution13.go
@@ -1,5 +1,23 @@
 package day_13
 
+import (
+	"slices"
+)
+
 func MultipleSum(lvl int, mgcNum []int) int {
-	return 0
+	var data []int
+	var finalSum int
+	for _, num := range mgcNum {
+		for i := 1; i < lvl; i++ {
+			if i%num == 0 {
+				if !slices.Contains(data, i) {
+					data = append(data, i)
+				}
+			}
+		}
+	}
+	for _, num := range data {
+		finalSum += num
+	}
+	return finalSum
 }
--- day_13/solution13_test.go
+++ day_13/solution13_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestMultipleSum(t *testing.T) {
 	finalSum := MultipleSum(20, []int{3, 5})
-
+	finalSum2 := MultipleSum(15, []int{2, 3, 4})
 	assert.Equal(t, 78, finalSum)
+	assert.Equal(t, 68, finalSum2)
 }
