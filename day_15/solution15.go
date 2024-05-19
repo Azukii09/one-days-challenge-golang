9ed3264baff2e7c17f4a54347fc82ca545608fcc
Azukii09 <azuknisianta@gmail.com>
2024-05-19 17:20:54 +0700

    add solution for problem day 15
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- day_15/solution15.go
+++ day_15/solution15.go
@@ -3,5 +3,21 @@ package day_15
 import "strconv"
 
 func Raindrop(number int) string {
-	return "number " + strconv.Itoa(number)
+	replacement := ""
+	pling := number%3 == 0
+	plang := number%5 == 0
+	plong := number%7 == 0
+	if pling {
+		replacement += "Pling"
+	}
+	if plang {
+		replacement += "Plang"
+	}
+	if plong {
+		replacement += "Plong"
+	}
+	if !(pling || plang || plong) {
+		return strconv.Itoa(number)
+	}
+	return replacement
 }
