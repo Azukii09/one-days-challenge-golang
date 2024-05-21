8838672ac8c48ebc43446fcbe1ec3575ee55eaaa
Azukii09 <azuknisianta@gmail.com>
2024-05-21 19:38:04 +0700

    add solution for problem day 17
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- day_17/solution17_test.go
+++ day_17/solution17_test.go
@@ -5,10 +5,9 @@ import (
 	"testing"
 )
 
-func NumOfStep(num int) int {
-	return num
-}
-
 func TestNumOfStep(t *testing.T) {
 	assert.Equal(t, 9, NumOfStep(12))
+	assert.Equal(t, 4, NumOfStep(16))
+	assert.Equal(t, 0, NumOfStep(1))
+	assert.Equal(t, 152, NumOfStep(1000000))
 }
