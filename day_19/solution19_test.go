cd1df274f598dd185468407cd60a88946ee54c19
Azukii09 <azuknisianta@gmail.com>
2024-05-23 17:36:30 +0700

    add test case problem day 19
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

---
+++ day_19/solution19_test.go
@@ -0,0 +1,18 @@
+package day_19
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func ISBN(str string) bool {
+	return false
+}
+func TestISBN(t *testing.T) {
+	assert.Equal(t, true, ISBN("3-598-21508-8"))
+	assert.Equal(t, false, ISBN("3-598-21508-9"))
+	assert.Equal(t, true, ISBN("3-598-21507-X"))
+	assert.Equal(t, false, ISBN("3-598-21507-A"))
+	assert.Equal(t, false, ISBN("4-598-21507-B"))
+	assert.Equal(t, true, ISBN("3598215088"))
+}
