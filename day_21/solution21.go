07247898de9213a12a69ac983e630218945b2d40
Azukii09 <azuknisianta@gmail.com>
2024-05-25 18:39:13 +0700

    add solution problem day 21
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- day_21/solution21.go
+++ day_21/solution21.go
@@ -1,1 +1,22 @@
 package day_21
+
+// Return all substrings of size n within s
+func All(n int, s string) []string {
+	// If asking for more than provided, get nothing
+	if n > len(s) {
+		return nil
+	}
+	subStrings := make([]string, 0, len(s)-n)
+	for i := 0; i+n <= len(s); i++ {
+		subStrings = append(subStrings, s[i:i+n])
+	}
+	return subStrings
+}
+
+func UnsafeFirst(n int, s string) string {
+	subStrings := All(n, s)
+	if subStrings == nil {
+		return ""
+	}
+	return subStrings[0]
+}
