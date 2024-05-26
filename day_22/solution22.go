659014ab1f296634c86582a189d18737f206afce
Azukii09 <azuknisianta@gmail.com>
2024-05-26 17:49:00 +0700

    add solution for problem day 22
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- day_22/solution22.go
+++ day_22/solution22.go
@@ -1,1 +1,18 @@
 package day_22
+
+func Sieve(limit int) []int {
+	composite := make([]bool, limit+1)
+	primes := make([]int, limit/2)
+	primeIndex := 0
+
+	for number := 2; number <= limit; number++ {
+		if !composite[number] {
+			primes[primeIndex] = number
+			primeIndex++
+			for idx := number + number; idx <= limit; idx += number {
+				composite[idx] = true
+			}
+		}
+	}
+	return primes[:primeIndex]
+}
